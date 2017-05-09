a3015104465f85c0a0b21c7cc0e89142a6ce0ada
micahdisney <micah.n.disney@gmail.com>
2017-05-08 22:02:32 -0500

    initial commit. recreated java codeup exercises repository with HelloWorld and io ConsoleExercises. also addind .gitignore.

---
+++ src/HelloWorld.java
@@ -0,0 +1,89 @@
+/**
+ * Created by micahdisney on 5/8/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 4;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string.";
+        System.out.println(myString);
+
+//        String myString = 'A';
+//        System.out.println(myString);
+
+        //does not work
+
+//        String myString = 3.14;
+//        System.out.println(myString);
+
+        // does not work
+
+//        long myNumber;
+//        System.out.println(myNumber);
+
+        //does not work
+
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        //does not work
+
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+        //does not work
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        //does not work
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //one does the addition before the number and one does it after.
+
+//        String class = "class";
+//        System.out.println(class);
+
+        //doesnt work
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+//        System.out.println(x);
+
+
+
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+//
+//
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+
+}
---
+++ src/io/ConsoleExercises.java
@@ -0,0 +1,42 @@
+package io;
+
+/**
+ * Created by micahdisney on 5/8/17.
+ */
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+//    System.out.println("Enter an integer: ");
+//    int integer = sc.nextInt();
+//    System.out.println("Your integer is " + integer);
+//
+//
+//
+//    System.out.println("Enter three words: ");
+//    String firstWord = sc.next();
+//    String secondWord = sc.next();
+//    String thirdWord = sc.next();
+//    System.out.println("Three words are " + firstWord + ", " + secondWord + ", " + thirdWord);
+
+//
+//    System.out.println("Enter a sentence: ");
+//    String sentence = sc.nextLine();
+//    System.out.println("Sentence: " + sentence);
+
+
+        System.out.println("What is the length of the classroom?");
+        double length = Double.parseDouble(sc.next());
+        System.out.println("What is the width of the classroom?");
+        double width = Double.parseDouble(sc.next());
+        System.out.println("area = " + length*width);
+        System.out.println("perimeter = " + (2*length) + (2*width));
+
+
+
+    }
+}
