1d0c73e5028bb43dc5738a1488405c01b049aeea
micahdisney <micah.n.disney@gmail.com>
2017-05-09 10:00:27 -0500

    updated ConsoleExercises after review

--- src/io/ConsoleExercises.java
+++ src/io/ConsoleExercises.java
@@ -4,6 +4,8 @@ package io;
  * Created by micahdisney on 5/8/17.
  */
 
+import jdk.internal.util.xml.impl.Input;
+
 import java.util.Scanner;
 
 public class ConsoleExercises {
@@ -11,30 +13,31 @@ public class ConsoleExercises {
 
         Scanner sc = new Scanner(System.in);
 
-//    System.out.println("Enter an integer: ");
-//    int integer = sc.nextInt();
-//    System.out.println("Your integer is " + integer);
-//
-//
-//
-//    System.out.println("Enter three words: ");
-//    String firstWord = sc.next();
-//    String secondWord = sc.next();
-//    String thirdWord = sc.next();
-//    System.out.println("Three words are " + firstWord + ", " + secondWord + ", " + thirdWord);
-
-//
-//    System.out.println("Enter a sentence: ");
-//    String sentence = sc.nextLine();
-//    System.out.println("Sentence: " + sentence);
+    System.out.println("Enter an integer: ");
+    int integer = sc.nextInt();
+    System.out.println("Your integer is " + integer);
 
 
-        System.out.println("What is the length of the classroom?");
-        double length = Double.parseDouble(sc.next());
-        System.out.println("What is the width of the classroom?");
-        double width = Double.parseDouble(sc.next());
-        System.out.println("area = " + length*width);
-        System.out.println("perimeter = " + (2*length) + (2*width));
+
+    System.out.println("Enter three words: ");
+    String firstWord = sc.next();
+    String secondWord = sc.next();
+    String thirdWord = sc.next();
+    sc.nextLine();
+    System.out.println("Three words are " + firstWord + ", " + secondWord + ", " + thirdWord);
+
+
+
+    System.out.println("Enter a sentence: ");
+    String sentence = sc.nextLine();
+    System.out.println("Sentence: " + sentence);
+
+    System.out.println("What is the length of the classroom?");
+    double length = Double.parseDouble(sc.next());
+    System.out.println("What is the width of the classroom?");
+    double width = Double.parseDouble(sc.next());
+    System.out.println("area = " + length*width);
+    System.out.println("perimeter = " + ((2*length) + (2*width)));
 
 
 
