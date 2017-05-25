a74cd3258aaebe8f0e000b3578d30d4397283307
micahdisney <micah.n.disney@gmail.com>
2017-05-25 12:10:28 -0500

    Exceptions and error handling completed

--- src/Java2/Input.java
+++ src/Java2/Input.java
@@ -20,7 +20,8 @@ public class Input {
     }
 
     public boolean yesNo() {
-        String userInput = this.scanner.next();
+        String userInput = this.getString();
+
         if ("Y".equalsIgnoreCase(userInput)){
             return true;
         }else{
@@ -28,37 +29,57 @@ public class Input {
         }
     }
 
-    public int getInt(int min, int max) {
-        int userInput = this.scanner.nextInt();
-        if(userInput >= min && userInput <= max){
-            return userInput;
-        }else{
-            System.out.println("Try again. Your number needs to be between " + min + " and " + max);
-            getInt(min, max);
-        }
-        return getInt();
-
-    }
-
     public int getInt(){
-        return this.scanner.nextInt();
+        try {
+            return Integer.valueOf(this.getString());
+        } catch (NumberFormatException e) {
+            System.out.print("Please enter an integer: ");
+            return getInt();
+        }
     }
 
-    public double getDouble(double min, double max) {
-        double userInput = this.scanner.nextDouble();
-        if (userInput >= min && userInput <= max) {
-            return userInput;
-        } else {
-            System.out.println("Try again. Your number needs to be between " + min + " and " + max);
-            getDouble(min, max);
+
+    public int getInt(int min, int max){
+        int num;
+        try {
+            String possibleNumber = this.getString();
+            num = Integer.valueOf(possibleNumber);
+        } catch (NumberFormatException e) {
+            System.out.print("Please enter *an integer* between " + min + " and " + max + ": ");
+            return getInt(min, max);
         }
-        return getDouble();
+
+        if(num >= min && num <= max){
+            return num;
+        }
+
+        System.out.println(num + " number out of range, try again");
+        return getInt(min, max);
     }
+
 
     public double getDouble() {
-        return this.scanner.nextDouble();
+        return scanner.nextDouble();
     }
 
+
+    public double getDouble(double min, double max){
+        double num = this.getDouble();
+        if(num >= min && num <= max){
+            return num;
+        }
+        System.out.println(num + " number out of range, try again");
+        return getDouble(min, max);
+
+    }
+
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.print("Please enter a number between 0-10: ");
+        int userInput = input.getInt(0, 10);
+        System.out.println("Your number is: " + userInput);
+    }
 
 
 
